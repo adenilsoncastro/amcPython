c9de2f2a1f2aa5535fabeca79fb48822cd0cee58
Adenilson Castro <adenilsoncastro@hotmail.com.br>
2020-01-24 14:43:24 -0300

    Features calculation/plotting features

--- features.py
+++ features.py
@@ -1,4 +1,12 @@
 import functions
+import pathlib
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os import listdir
+from os.path import join
 
 def calculate_features(input_signal):
     f1 = functions.std_deviation((abs(functions.instantaneous_phase(input_signal))))
@@ -12,3 +20,43 @@ def calculate_features(input_signal):
     f9 = functions.std_deviation(functions.instantaneous_cn_absolute(input_signal))
     result = [f1, f2, f3, f4, f5, f6, f7, f8, f9]
     return result
+
+def plotFeatures():
+    #sns.set()
+    modulations = ['8PSK', '16PSK', '16QAM', '64QAM', '256QAM', 'BPSK', 'QPSK'] #TODO: receive these as functions parameters
+    snr = np.linspace(-14, 20, 18, dtype=int)
+    figure_folder = pathlib.Path("./figures/")
+
+    dataFolder = pathlib.Path("./features.pickle")
+    with open(dataFolder, 'rb') as handle:
+        data = pickle.load(handle)
+    
+    meanFeatures = np.zeros((len(data), len(data[0]), len(data[0][0][0]))) #mod/snr/ft1...ftn
+    #Calculates the mean of all features
+    for mod in range(len(data)):
+        for snr in range(len(data[0])):
+            for feature in range(len(data[0][0][0])): 
+                aux = np.array([])
+                for frame in range(len(data[0][0])):
+                    aux = np.append(aux, data[mod][snr][frame][feature])
+                meanFeatures[mod][snr][feature] = np.mean(aux)    
+
+    for feature in range(len(meanFeatures[mod][snr])):
+        plt.figure(figsize=(6.4, 3.6), dpi=300)
+        plt.xlabel('SNR')
+        plt.ylabel('Value')
+        plt.title('Feature {}'.format(feature))        
+        for mod in range(len(meanFeatures)):
+            aux = np.array([])
+            for snr in range(len(meanFeatures[mod])):
+                aux = np.append(aux, meanFeatures[mod][snr][feature])
+            plt.plot(aux, label=modulations[mod])   
+            #df = pd.DataFrame(aux)
+            #sns.relplot(data=df, kind="line")
+        plt.legend(loc='best')
+        plt.savefig(join(figure_folder,'Feature {}'.format(feature) + ".png"), bbox_inches='tight', dpi=300)
+    
+    #TODO:check if features calculations is correct
+if __name__ == "__main__":
+    print("Check main file for features specifications!")
+    plotFeatures()
--- main.py
+++ main.py
@@ -9,39 +9,45 @@ from os.path import isfile, join
 
 dataFolder = pathlib.Path("../dataset/201801a_splitted/")
 onlyFiles = [f.split(".")[0] for f in listdir(dataFolder) if isfile(join(dataFolder, f))]  # List all data files available
+frames = 1000  # Up to 4096
 frameSize = 1024  # Up to 1024
-frames = 100  # Up to 4096
 nFeatures = 9
 
-modulations = ['16QAM', 'BPSK']
-snr = ['0']
+modulations = ['8PSK', '16PSK', '16QAM', '64QAM', '256QAM', 'BPSK', 'QPSK']
+snr = np.linspace(-14, 20, 18, dtype=int)
 
-# Reads the pickles and stores in 'data' array in the following format:
-data = np.empty((len(modulations), len(snr), frames,
-                 frameSize), dtype=np.complex)
-for idx, mod in enumerate(modulations):
-    print("Extracting data from {}".format(mod))
-    temp = np.array([])
-    for idx_, snr_ in enumerate(snr):
-        for f in onlyFiles:
-            if f.split("_")[0] == mod and f.split("_")[1] == snr_:
-                with open(join(dataFolder, f + ".pickle"), 'rb') as handle:
-                    signal_data = pickle.load(handle)
-                    signal = np.empty((frames, frameSize), dtype=np.complex)
-                    for frame in range(frames):
-                        for sample in range(frameSize):
-                            signal[frame][sample] = complex(
-                                signal_data[frame][sample][0], signal_data[frame][sample][1])
-                    data[idx, idx_, :, :] = signal[:, :]
-                    break
+def generateFeaturesData(modulations, snr):
+    # Reads the pickles and stores in 'data' array in the following format:
+    data = np.empty((len(modulations), len(snr), frames,
+                    frameSize), dtype=np.complex)
+    for idx, mod in enumerate(modulations):
+        print("Extracting data from {}".format(mod))
+        for idx_, snr_ in enumerate(snr):
+            for f in onlyFiles:
+                if f.split("_")[0] == mod and f.split("_")[1] == str(snr_):
+                    with open(join(dataFolder, f + ".pickle"), 'rb') as handle:
+                        signal_data = pickle.load(handle)
+                        signal = np.empty((frames, frameSize), dtype=np.complex)
+                        for frame in range(frames):
+                            for sample in range(frameSize):
+                                signal[frame][sample] = complex(
+                                    signal_data[frame][sample][0], signal_data[frame][sample][1])
+                        data[idx, idx_, :, :] = signal[:, :]
+                        break
 
-plt.plot(data[0][0][0][:].real, color='blue')
-plt.plot(data[0][0][0][:].imag, color='green')
-plt.title('i/q data')
-plt.show()
+    #Calculate features and save it to a pickle file
+    features = np.zeros((len(modulations), len(snr), frames, nFeatures))
 
-features = np.zeros((frames, nFeatures))
-for idx in range(nFeatures):
-    features[idx][:] = ft.calculate_features(data[0][0][0][:])
+    for mod in range((len(data))):
+        print("Extracting features from {}".format(modulations[mod]))
+        for snr in range(len(data[mod])):
+            for frame in range(len(data[mod][snr])):
+                features[mod][snr][frame][:] = ft.calculate_features(data[mod][snr][frame][:])
 
+    with open('features.pickle', 'wb') as handle:
+        pickle.dump(features, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    del features
+
+#generateFeaturesData(modulations, snr)
+ft.plotFeatures()
 print('well done!')
