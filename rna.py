e78fee58eca9947f6e6f010dbaf6dab998f73c59
Adenilson Castro <adenilsoncastro@hotmail.com.br>
2020-08-21 18:20:47 -0300

    update

--- rna.py
+++ rna.py
@@ -271,6 +271,67 @@ def evaluate_rna(id="foo", test_size=500):  # Make a prediction using some sampl
             print("Weights file not found. Saving it into RNA folder")
             model.save_weights(str(join(rna_folder, 'weights-' + id + '.h5')))
 
+def quantize_rna(id):
+    fig_folder = pathlib.Path(join(os.getcwd(), "figures"))
+    data_folder = pathlib.Path(join(os.getcwd(), "mat-data", "pickle"))
+    rna_folder = pathlib.Path(join(os.getcwd(), 'rna'))
+
+    print("Starting model quantization...")
+    rna = join(str(rna_folder), "rna-" + id + ".h5")
+    model = load_model(rna)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optmizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
+
+    with tf.io.gfile.GFile(join(rna_folder, 'rna-' + id + '_lite.h5'), 'wb') as f:
+        f.write(tflite_model)
+
+    print("Starting model evaluation...")
+    result = np.zeros((len(modulations), number_of_snr))
+    interpreter = tf.lite.Interpreter(model_path=join(rna_folder, 'rna-' + id + '_lite.h5'))
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    input_shape = input_details[0]['shape']
+
+    for i, mod in enumerate(test_data_files):
+        print("Evaluating {}".format(mod.split("_")[0]))
+        
+        with open(join(data_folder, mod), 'rb') as evaluating_data:
+            data = pickle.load(evaluating_data)
+        
+        for j, snr in enumerate(snr_list):
+            right_label = []
+            predicted_label = []
+            for _ in range(500):
+                random_sample = np.random.choice(data[snr - (21 - number_of_snr)][:].shape[0], 1)
+                data_test = [data[snr - (21 - number_of_snr)][i] for i in random_sample]
+                data_test = normalize(data_test, norm='l2')                
+                partial_right_label = info_json['modulations']['labels'][mod.split("_")[0]]
+                data_test_float32 = data_test.astype(np.float32)
+                interpreter.set_tensor(input_details[0]['index'], data_test_float32)
+                interpreter.invoke()
+                output_data = interpreter.get_tensor(output_details[0]['index'])
+                partial_predicted_label = np.argmax(output_data)
+                right_label.append(partial_right_label)
+                predicted_label.append(partial_predicted_label)
+
+            accuracy = accuracy_score(right_label, predicted_label)          
+            result[i][j] = accuracy
+    
+    figure = plt.figure(figsize=(8, 4), dpi=150)
+    plt.title("Accuracy - Lite model")
+    plt.ylabel("Right prediction")
+    plt.xlabel("SNR [dB]")
+    plt.xticks(np.arange(number_of_snr), [info_json['snr']['values'][i] for i in info_json['snr']['using']])
+    for item in range(len(result)):
+        plt.plot(result[item], label=modulations[item])
+    plt.legend(loc='best')
+    plt.savefig(join(fig_folder, "accuracy_lite_" + id + ".png"), bbox_inches='tight', dpi=300)
+
+    figure.clf()
+    plt.close(figure)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RNA argument parser')
@@ -295,8 +356,9 @@ if __name__ == '__main__':
         layer_size_hl2=int(arguments.layer_size_hl2),
         layer_size_hl3=int(arguments.layer_size_hl3)
     )
-    wandb.init(project="amcpy-team", config=hyperparameterDefaults)
+    wandb.init(project="amcpy", config=hyperparameterDefaults)
     config = wandb.config
 
     #evaluate_rna(id="96ec22de")
     train_rna(config)
+    #quantize_rna(id="96ec22de")
